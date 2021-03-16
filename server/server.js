6b5389a46bab0b083ebe636dee95cfd7fceaf2fb
Rocio.Gonzalez <180707@utags.edu.mx>
2021-03-16 08:53:42 -0600

    Conexion lista a BD

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require ('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -61,6 +62,16 @@ app.delete('/usuario/:id', function(req, res){
         id: id
     });
 });
+
+mongoose.connect('mongodb://localhost:27017/coffe_shop', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+     useCreateIndex: true
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+}); 
 
 app.listen(process.env.PORT, () => {
     console.log('El servidor esta en linea por el puerto', process.env.PORT); 
